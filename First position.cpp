7dac9e2b29ed4ecb50c3831ee347a382338b6a2b
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-25 19:19:32 +0700

    Create First position.cpp

---
+++ First position.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int binarySearch(vector<int> a, int l, int r , int x)
+{
+	int res = -1;
+	while (l <= r)
+	{
+		int mid = (l + r) / 2;
+		if (a[mid] == x)
+		{
+			res = mid;
+			r = mid - 1;
+		}
+		else if (a[mid] > x)
+		{
+			r = mid - 1;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+	}
+	return res;
+}
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	cout << binarySearch(a, 0, n - 1, x);
+}
