c4637ed614ba37649a7b3448f5a696b1e7558f60
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2024-12-25 20:55:48 +0700

    Update UpperBound.cpp

--- UpperBound.cpp
+++ UpperBound.cpp
@@ -4,42 +4,34 @@
 #include <algorithm>
 using namespace std;
 
-int upperbound(vector<int> a, int l, int r, int x)
+int upperbound(vector<int>a, int l, int r, int x)
 {
 	int res = -1;
 	while (l <= r)
 	{
 		int m = (l + r) / 2;
-		if (a[m] <= x)
+		if (a[m] > x)
+		{
+			r = m - 1;
+		}
+		else if (a[m] < x)
 		{
 			res = m;
 			l = m + 1;
 		}
-		else
-		{
-			r = m - 1;
-		}
 	}
 	return res;
 }
-
 int main()
 {
-	int n, x;
-	cin >> n >> x;
+	int n;
+	cin >> n;
 	vector<int> a(n);
 	for (int i = 0; i < n; i++)
 	{
 		cin >> a[i];
 	}
 	sort(a.begin(), a.end());
-	int pos = upperbound(a, 0, n - 1, x);
-	if (pos == -1)
-	{
-		cout << "NOT FOUND" << endl;
-	}
-	else
-	{
-		cout << a[pos] << endl;
-	}
+	int pos = upperbound(a, 0, n - 1, 3);
+	cout << a[pos];
 }
